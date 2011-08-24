0d6b1448772cf1315dcd569326a3eaf96d754873
Joshua Gao <jmg116@gmail.com>
2011-08-24 07:52:40 +0000

    Initial commit, :effort:

---
+++ tlbw.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          TLBW
+// @description   :effort:
+// @include       http://teamliquid.net/*
+// @include		  http://*.teamliquid.net/*
+// ==/UserScript==
+
+document.forms['frm_tlpd_search'].type.children[3].selected = true;
