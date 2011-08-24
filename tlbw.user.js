c5704de3e8734596be50c89a9d7758436b2dfe75
Joshua Gao <jmg116@gmail.com>
2011-08-24 12:53:55 +0000

    Changed to blacklisting pages; made script run on DOMContentLoaded on Opera

--- tlbw.user.js
+++ tlbw.user.js
@@ -1,18 +1,46 @@
 // ==UserScript==
 // @name          TLBW
 // @description   Userscript to make teamliquid more useful for Brood War fans
-// @version       2.0
-// @include       http://teamliquid.net/
-// @include       http://teamliquid.net/index.php
-// @include       http://teamliquid.net/forum/*
-// @include       http://www.teamliquid.net/
-// @include	      http://www.teamliquid.net/index.php
-// @include       http://www.teamliquid.net/forum/*
+// @version       2.1
+// @include       http://teamliquid.net/*
+// @include       http://www.teamliquid.net/*
+// @exclude	      http://teamliquid.net/sc2/*
+// @exclude	      http://teamliquid.net/store/*
+// @exclude	      http://teamliquid.net/tlfe/*
+// @exclude	      http://teamliquid.net/tournaments/*
+// @exclude	      http://teamliquid.net/vods/*
+// @exclude	      http://www.teamliquid.net/sc2/*
+// @exclude	      http://www.teamliquid.net/store/*
+// @exclude	      http://www.teamliquid.net/tlfe/*
+// @exclude	      http://www.teamliquid.net/tournaments/*
+// @exclude	      http://www.teamliquid.net/vods/*
 // ==/UserScript==
-
+ 
+/* Changelog:
+ *       2.1:     Added all of teamliquid and blacklisted specific pages to run the script on, instead of whitelisting every individual page
+ *                Merged changes by kuroshiroi to make the script run on DOMContnetLoaded on Opera (should do this by default on Greasemonkey, but no downside in making it do so anyway)
+ *
+ *       2.0:     Abstracted the section relocation stuff
+ */
+ 
 /* Sections: news, general, sc2, bw, games, blogs, replays, calendar, streams, tlpd, liquipedia, tsl, poll */
 
 function main() {
+	var news = new Section("nav_news_left_mid", 1, false);
+	var general = new Section("nav_general", 1, false);
+	var sc2 = new Section("nav_starcraft2", 1, false);
+	var bw = new Section("nav_broodwar", 1, false);
+	var games = new Section("nav_games", 1, false);
+	var blogs = new Section("nav_blogs", 1, false);
+	var replays = new Section("nav_replays", 1, false);
+	
+	var calendar = new Section("nav_calendar", 2, true);
+	var streams = new Section("nav_streams", 1, true);
+	var tlpd = new Section("nav_tlpd", 1, true);
+	var liquipedia = new Section("nav_wiki", 1, true);
+	var tsl = new Section("nav_tslforum", 1, true);
+	var poll = new Section("nav_poll", 1, true);
+	
 	/* Move the Brood War forums above the SC2 forums */
 	move_section(bw, sc2);
 	
@@ -42,20 +70,6 @@ function main() {
 	rank2.parentNode.removeChild(rank2);
 }
 
-var nextObject = function(el) {
-	var n = el;
-	do n = n.nextSibling;
-	while (n && n.nodeType != 1);
-	return n;
-}
- 
-var previousObject = function(el) {
-	var p = el;
-	do p = p.previousSibling;
-	while (p && p.nodeType != 1);
-	return p;
-}
-
 function Section(id, count, on_right) {
 	this.on_right = on_right;
 	this.elements = new Array();
@@ -66,21 +80,6 @@ function Section(id, count, on_right) {
 		this.elements.push(nextObject(link));
 	}
 }
-
-var news = new Section("nav_news_left_mid", 1, false);
-var general = new Section("nav_general", 1, false);
-var sc2 = new Section("nav_starcraft2", 1, false);
-var bw = new Section("nav_broodwar", 1, false);
-var games = new Section("nav_games", 1, false);
-var blogs = new Section("nav_blogs", 1, false);
-var replays = new Section("nav_replays", 1, false);
-
-var calendar = new Section("nav_calendar", 2, true);
-var streams = new Section("nav_streams", 1, true);
-var tlpd = new Section("nav_tlpd", 1, true);
-var liquipedia = new Section("nav_wiki", 1, true);
-var tsl = new Section("nav_tslforum", 1, true);
-var poll = new Section("nav_poll", 1, true);
 
 var remove_section = function(section) {
 	for (var x = 0; x < section.elements.length; x++) {
@@ -102,4 +101,33 @@ var move_section = function(from, to) {
 	prepend_section(from, to);
 }
 
-main();
+/* Adapted from http://v3.thewatchmakerproject.com/journal/329/ */
+var nextObject = function(el) {
+	var n = el;
+	do n = n.nextSibling;
+	while (n && n.nodeType != 1);
+	return n;
+}
+ 
+var previousObject = function(el) {
+	var p = el;
+	do p = p.previousSibling;
+	while (p && p.nodeType != 1);
+	return p;
+}
+
+if (document.readyState==="loading")  {
+	if (window.addEventListener)
+		window.addEventListener("DOMContentLoaded", main, false);
+	else if (window.attachEvent)
+		window.attachEvent("onload", main);
+}
+else if (document.readyState==="complete") {
+	main();
+}
+else {
+	if (window.addEventListener)
+		window.addEventListener("load", main, false);
+	else if (window.attachEvent)
+		window.attachEvent("onload", main);
+}
